224d83f19393821a2b9c99ce7bebbb48edfa3620
Michael Lohr <michael@lohr.dev>
2021-12-18 18:52:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Chocolatey just package
+
+[![Chocolatey](https://img.shields.io/chocolatey/v/just?include_prereleases)](https://community.chocolatey.org/packages/just)
+
+## Setup
+
+[just](https://community.chocolatey.org/packages/just) can be installed using [Chocolatey](https://chocolatey.org/) using the following command:
+
+```bash
+choco install -y just
+```
+
+## Release a new version
+
+1. Change url and checksum in `tools/chocolateyinstall.ps1`
+2. Adjust version number in `just.nuspec`
+3. Run `choco pack` in the current directory
+4. Run `choco push .\{file}.nupkg --source https://push.chocolatey.org/`
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'just'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64       = 'https://github.com/casey/just/releases/download/0.10.5/just-0.10.5-x86_64-pc-windows-msvc.zip'
+$bin         = $toolsDir + '\just.exe'
+
+$packageArgs = @{
+    packageName   = $packageName
+    url64bit      = $url64
+
+    softwareName  = 'just*'
+
+    unzipLocation = $toolsDir
+
+    checksum64    = 'EF5421ED3B3663F58A804D1F3D8F865318FC1CB89E87CC78B28B00DFD1F41A6B'
+    checksumType64= 'sha256'
+
+    validExitCodes= @(0)
+}
+Install-ChocolateyZipPackage @packageArgs
