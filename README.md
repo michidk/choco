8719ec64535e61c5f58525972503835e6cc34afc
Michael Lohr <michael@lohr.dev>
2022-02-03 12:32:06 +0100

    Update to Version 0.10.7

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ choco install -y just
 
 ## Release a new version
 
-1. Change url and checksum in `tools/chocolateyinstall.ps1`
+1. Change url and checksum (calculated with PowerShell: `Get-FileHash`) in `tools/chocolateyinstall.ps1`
 2. Adjust version number in `just.nuspec`
 3. Run `choco pack` in the current directory
 4. Run `choco push .\{file}.nupkg --source https://push.chocolatey.org/`
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,20 +2,16 @@
 
 $packageName = 'just'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/casey/just/releases/download/0.10.5/just-0.10.5-x86_64-pc-windows-msvc.zip'
+$url64       = 'https://github.com/casey/just/releases/download/0.10.7/just-0.10.7-x86_64-pc-windows-msvc.zip'
 $bin         = $toolsDir + '\just.exe'
 
 $packageArgs = @{
     packageName   = $packageName
     url64bit      = $url64
 
-    softwareName  = 'just*'
-
     unzipLocation = $toolsDir
 
-    checksum64    = 'EF5421ED3B3663F58A804D1F3D8F865318FC1CB89E87CC78B28B00DFD1F41A6B'
+    checksum64    = '47C353F7925EFE6CE989362C249ECD6711EC0D2624B5ACDE2FF81C260C0C3994'
     checksumType64= 'sha256'
-
-    validExitCodes= @(0)
 }
 Install-ChocolateyZipPackage @packageArgs
