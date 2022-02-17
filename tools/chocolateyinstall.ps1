fecd456d113ec8b2d9cb94e1fbb6dabb1022bdd7
Michael Lohr <michael@lohr.dev>
2022-02-17 10:44:13 +0100

    Update to Version 0.11.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'just'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/casey/just/releases/download/0.10.7/just-0.10.7-x86_64-pc-windows-msvc.zip'
+$url64       = 'https://github.com/casey/just/releases/download/0.11.2/just-0.11.2-x86_64-pc-windows-msvc.zip'
 $bin         = $toolsDir + '\just.exe'
 
 $packageArgs = @{
@@ -11,7 +11,7 @@ $packageArgs = @{
 
     unzipLocation = $toolsDir
 
-    checksum64    = '47C353F7925EFE6CE989362C249ECD6711EC0D2624B5ACDE2FF81C260C0C3994'
+    checksum64    = '8293CC38EAB2BA5570A1E1D91699C1D8441DCCBACF04EBC5B979D14E917E38CE'
     checksumType64= 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
