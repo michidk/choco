9ca4830ff0a4bcdb1f872bd4a4e0c8e7941620bf
Michael Lohr <michael@lohr.dev>
2023-06-18 21:36:32 +0200

    Add vscli

---
+++ vscli/README.md
@@ -0,0 +1,11 @@
+# vscli
+
+[![MIT License](https://img.shields.io/crates/l/vscli)](https://choosealicense.com/licenses/mit/) [![Continuous integration](https://github.com/michidk/vscli/workflows/Continuous%20Integration/badge.svg)](https://github.com/michidk/vscli/actions) [![Crates.io](https://img.shields.io/crates/v/vscli)](https://crates.io/crates/vscli) [![Homebrew](https://img.shields.io/badge/homebrew-available-blue?style=flat)](https://github.com/michidk/homebrew-tools/blob/main/Formula/vscli.rb)
+
+A CLI tool to launch vscode projects, which supports [devcontainers](https://containers.dev/).
+
+## Features
+
+- A shorthand for launching vscode projects
+- Detects whether a project is a [devcontainers](https://containers.dev/) project, and launches the devcontainer instead
+- Supports the [insiders](https://code.visualstudio.com/insiders/) version of vscode
---
+++ vscli/update.ps1
@@ -0,0 +1,36 @@
+Import-Module au
+
+$releases = "https://github.com/michidk/vscli/releases/latest"
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+            "(?i)(\s+checksum64:).*"     = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_GetLatest {
+    $request = [System.Net.WebRequest]::Create($releases)
+    $request.AllowAutoRedirect = $false
+    $response = $request.GetResponse()
+    $downloadUrl = $response.GetResponseHeader("Location")
+
+    $version = $downloadUrl -Split '/' | Select-Object -Last 1
+
+    @{
+        URL64        = "https://github.com/michidk/vscli/releases/download/$version/vscli-x86_64-pc-windows-msvc.zip"
+        Version = $version.replace("v", "")
+        ReleaseNotes = "https://github.com/michidk/vscli/releases/tag/$version"
+    }
+}
+
+update -ChecksumFor None
