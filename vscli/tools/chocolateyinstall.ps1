8e1d7bde51d4aec150e5af39747cf618dc7ddca0
Michael Lohr <michael@lohr.dev>
2023-09-24 11:27:51 +0200

    fix(typst/vscli): Add install script (fixes #2)

---
+++ vscli/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = 'vscli'
+    fileFullPath64 = "$toolsDir\vscli-x86_64-pc-windows-msvc.zip"
+    destination    = $toolsDir
+}
+Get-ChocolateyUnzip @packageArgs
+
+# Don't need zip file anymore
+Remove-Item $toolsDir\*.zip -ea 0 -force
